fae509d83d2fa44924a86b50ed555e438cd87c8c
Dawid Pych <dawidpych@wp.pl>
2015-04-14 00:22:42 +0200

    testy

---
+++ src/example.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of example
+ *
+ * @author dawid
+ */
+class example {
+    public function dodaj($a,$b) {
+        return $a+$b;
+    }
+}
